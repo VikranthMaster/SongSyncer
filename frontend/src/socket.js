c1d1a10f9d283ae545beccec46a3e81aec935a7b
Vikranth <69100217+VikranthMaster@users.noreply.github.com>
2025-12-20 18:20:07 +0530

    Update socket connection URL

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://e0e181578785.ngrok-free.app", {
+export const socket = io("https://d77f8f4379e5.ngrok-free.app", {
   transports: ["websocket"], // important
 });
